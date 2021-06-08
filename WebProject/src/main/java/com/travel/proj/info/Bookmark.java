24cde91c241f193d15f477c8ce8effa1f2ca6aa7
slinfo <slinfo@DESKTOP-O62A677>
2021-06-08 17:01:33 +0900

    Update domain files (Bookmark.java, InfoBoard.java)

--- WebProject/src/main/java/com/travel/proj/info/Bookmark.java
+++ WebProject/src/main/java/com/travel/proj/info/Bookmark.java
@@ -1,7 +1,20 @@
 package com.travel.proj.info;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
 public class Bookmark {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "book_sequence")
+	@SequenceGenerator(name = "book_sequence", sequenceName = "seq_book", allocationSize=1)
+	private int bookNum;
 	// Member와 InfoBoard를 참조함
+	private int memNum;
+	private int boardNum;
+	
+	
 	
 }
--- WebProject/src/main/java/com/travel/proj/info/InfoBoard.java
+++ WebProject/src/main/java/com/travel/proj/info/InfoBoard.java
@@ -8,10 +8,11 @@ import javax.persistence.SequenceGenerator;
 
 @Entity
 public class InfoBoard {
+	// normal board
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "info_sequence")
-	@SequenceGenerator(name = "info_sequence", sequenceName = "info_board", allocationSize=1)
+	@SequenceGenerator(name = "info_sequence", sequenceName = "seq_info", allocationSize=1)
 	private int boardNum;
 	private int viewCount;
 	
