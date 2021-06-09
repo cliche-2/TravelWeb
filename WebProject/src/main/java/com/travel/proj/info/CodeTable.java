3d7d68272ece987ab4cbd7ef3c0ef0fcf8da7597
slinfo <slinfo@DESKTOP-O62A677>
2021-06-09 17:35:36 +0900

    INFO UPDATED

---
+++ WebProject/src/main/java/com/travel/proj/info/CodeTable.java
@@ -0,0 +1,7 @@
+package com.travel.proj.info;
+
+public class CodeTable {
+	
+	// 사용하는 정보 코드 값 저장하는 클래스
+
+}
--- WebProject/src/main/java/com/travel/proj/info/DataController.java
+++ WebProject/src/main/java/com/travel/proj/info/DataController.java
@@ -19,6 +19,8 @@ public class DataController {
 		Map map = new HashMap();
 		boolean result = false;
 		
+		// vue에서 필요한 일정 값 받아오기 
+		
 		// api 콜하기
 		
 		
